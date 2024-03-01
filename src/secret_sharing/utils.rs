87ad6ddb9af6213eca0626438860e4b3ee03824d
Rohit Sinha <rohit@swirldslabs.com>
2024-03-01 07:16:37 -0800

    removing adss implementation
    Signed-off-by: Rohit Sinha <rohit@swirldslabs.com>

--- src/secret_sharing/utils.rs
+++ src/secret_sharing/utils.rs
@@ -16,12 +16,9 @@
  */
 
 use aes_gcm::{aead::Aead, Aes128Gcm, Nonce, Key};
-use aes::{Aes128, Block};
 use aes::cipher::{
     typenum::*,
-    KeyInit, 
-    BlockEncryptMut,
-    generic_array::GenericArray
+    KeyInit,
 };
 use sha2::{Sha256, Digest};
 use crate::secret_sharing::*;
@@ -30,39 +27,6 @@ pub fn compute_sha256_hash(input: &[u8]) -> Vec<u8> {
     let mut hasher = Sha256::new();
     hasher.update(input);
     hasher.finalize().to_vec()
-}
-
-#[allow(dead_code)]
-pub fn prg_seed_expansion(rand: &[u8]) -> Option<[u8; 32]> {
-    if rand.len() != 16 { //expecting only 128 bit keys
-        return None;
-    }
-
-    let mut seed : [u8; 32] = [0u8; 32];
-    for i in 0..16 {
-        seed[2*i] = rand[i];
-        seed[2*i + 1] = rand[i];
-    }
-    Some(seed)
-}
-
-#[allow(dead_code)]
-pub fn one_time_pad_rand(
-    rand: &[u8; λ], 
-    key: &[u8; λ]
-) -> Vec<u8> {
-    let mut cipher = Aes128::new(&GenericArray::from_slice(key));
-
-    let mut block = Block::default();
-    cipher.encrypt_block_mut(&mut block);
-
-    let ciphertext_block : Vec<u8> = rand
-            .iter()
-            .zip(block.iter())
-            .map(|(x1, x2)| x1 ^ x2)
-            .collect();
-
-    ciphertext_block
 }
 
 pub fn encrypt_message(msg: &[u8], key: &[u8; λ]) -> Vec<u8> {
